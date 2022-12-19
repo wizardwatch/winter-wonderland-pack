03057451e1d6d1b419d1c97289feee4ae3ce65f8
wizardwatch <48140117+wizardwatch@users.noreply.github.com>
2022-12-19 06:55:50 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ mkdir -p /etc/minecraft
 chown /etc/minecraft minecraft
 wget -nc -q -O "/etc/minecraft/server.jar" "https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar"
 wget -nc -q -O "/etc/minecraft/packwiz-installer-bootstrap.jar" "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar"
-exec /usr/bin/env java -jar /etc/minecraft/packwiz-installer-boostrap.jar
-exec /usr/bin/env java -Xmx6G -jar /etc/minecraft/fabric-server-launch.jar nogui
+java -jar /etc/minecraft/packwiz-installer-boostrap.jar
+java -Xmx6G -jar /etc/minecraft/fabric-server-launch.jar nogui
